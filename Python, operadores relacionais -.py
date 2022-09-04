2a9708ecc186c0d9eb7e0fe6b8764312da614975
Matheus <103674883+IleckMatheus@users.noreply.github.com>
2022-09-04 12:02:55 -0300

    Operadores Relacionais

---
+++ Python, operadores relacionais -.py
@@ -0,0 +1,35 @@
+#operadores relacionais
+a = 1
+b = 2
+c = 2
+print("a igual b - ", a==b)
+print("a diferente b -", a!=b)
+print("b maior que a -", b>a)
+print("a menor ou igual a b - ", a <= b)
+print("c maior ou igual a b -", c>=b)
+
+#Exemplo (Menezes, 2019)
+nota = 8
+média = 7
+aprovado = nota >= média
+print("resultado da verificação da aprovação =", aprovado)
+
+#operadores lógicos
+# not = não
+# and = e 
+# or = ou
+
+# declaração de variáveis do tipo lógico:
+V = True
+F = False
+        
+    #Exemplo para operador "not"(Menezes, 2019)
+print("complemento de true = ", not V)
+print("complemento de false -", not F)
+    
+    #Exemplo para operador "e" (Menezes, 2019):
+print("true and true = ", V and V)
+print("True and False = ", V or F)
+print("False or False =", F or F)
+
+        
