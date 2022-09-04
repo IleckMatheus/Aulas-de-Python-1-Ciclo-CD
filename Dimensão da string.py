0634d3ee41df6eed89eae454b239a446a63cdfb7
Matheus <103674883+IleckMatheus@users.noreply.github.com>
2022-09-04 12:04:30 -0300

    dimensão da string

---
+++ Dimensão da string.py
@@ -0,0 +1,19 @@
+a ="Matheus"
+print("dimensão da string a:", len(a))
+print("a dimensão de string com espaço de (a b):", len("a b"))
+print("dimensao de string vazia: ", len(""))
+
+#acesso de caracteres de strings
+print("primeira letra de Matheus: ", a[0])
+c = "ab"
+print("valor entre a e b = ", c[1])
+d ="abc"
+print("valor entre a e c = ", d[1])
+
+#string concatenadas
+e=" Ileck"
+print("nome do programador", a+e)
+print ("T*5")
+print("-*" * 10 + "-")
+
+
