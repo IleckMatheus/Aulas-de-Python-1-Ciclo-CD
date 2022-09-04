919ddf5f5034eeb9264070aca370c00b891ff24c
Matheus <103674883+IleckMatheus@users.noreply.github.com>
2022-09-04 13:25:34 -0300

    Exercício 8 - em Python, chances de chuva

---
+++ Temperatura.py
@@ -0,0 +1,8 @@
+temp = int(input("Digite a Temperatura: "))
+umd_rel_ar = int(input("Digite a Umidade Relativa do Ar: "))
+
+if temp >= 28 and umd_rel_ar >= 50:
+    print("Irá chover amanhã")
+else:
+    print("amanhã o dia será sem chuvas")
+
