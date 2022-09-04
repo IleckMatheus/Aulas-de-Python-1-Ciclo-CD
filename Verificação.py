3698b95bc091799d2ac456b361403725b51ad931
Matheus <103674883+IleckMatheus@users.noreply.github.com>
2022-09-04 12:02:28 -0300

    Verificação das informações sobre o Matheus

---
+++ Verificação.py
@@ -0,0 +1,16 @@
+#Entrada de Dados
+
+idade = int(input("idade: "))
+nome = input("nome: ")
+salário = float(input("digite salário: R$"))
+
+
+#Saida de Dados
+print(nome)
+print(idade)
+print(salário)
+
+#Verificação dos Dados
+print("verificando idade: ", idade < 55)
+print("verificando nome: ", nome == "Matheus")
+print("verificando salário: ", salário >= 1698.88)
