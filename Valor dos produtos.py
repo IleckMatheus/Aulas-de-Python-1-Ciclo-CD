37d9a96deb5edd70e7ffcc3f21e01c5083e6fbd1
Matheus <103674883+IleckMatheus@users.noreply.github.com>
2022-09-16 09:52:50 -0300

    Update Valor dos produtos.py

--- Valor dos produtos.py
+++ Valor dos produtos.py
@@ -1,10 +1,3 @@
-# -*- coding: utf-8 -*-
-"""
-Created on Sun Sep  4 12:04:13 2022
-
-@author: mathe
-"""
-
 arroz = 5
 feijao = 3
 acucar = 2
@@ -16,7 +9,6 @@ banana = 18
 couve_flor = 2
 leite_po = 2
 
-
 vl_arroz = float(input("Digite o valor do kg do arroz: "))
 vl_feijao = float(input("Digite o valor do kg do feijão: "))
 vl_acucar = float(input("Digite o valor do kg do açúcar: "))
@@ -26,41 +18,33 @@ vl_bolachaAS = float(input("Digite o valor do pacote de Bolacha de Água e Sal: 
 vl_bolachaM = float(input("Digite o valor do pacote de Maizena: "))
 vl_couve = float(input("Digite o valor do maço de Couve-Flor: "))
 vl_leitepo = float(input("Digite o valor da lata Leite em Pó: "))
-print(" ")
 
-print(" =======================================  ")
+print("\n =======================================  \n")
 
-print(" ")
+print("O valor dos produtos são: \n")
 
-print("O valor dos produtos são")
+print("O valor do KG de arroz é de: ", vl_arroz,"\n")
 
-print("O valor do KG de arroz é de: ", vl_arroz)
-print(" ")
-print("O valor do KG do Feijão é de: ", vl_feijao)
-print(" ")
-print("O valor do KG do Açúcar é de: ", vl_acucar)
-print(" ")
-print("O valor do KG da Batata é de: ", vl_batata)
-print(" ")
-print("O valor do Litro de Óleo é de: ", vl_oleo)
-print(" ")
-print("O valor do PACOTE de Bolacha de água e sal é de: ", vl_bolachaAS)
-print(" ")
-print("O valor do PACOTE de Bolacha de maizena é de: ", vl_bolachaM)
-print(" ")
-print("O valor do MAÇO de Couve-flor é de: ", vl_couve)
-print(" ")
-print("O valor da LATA de Leite em Pó é de: ", vl_leitepo)
+print("O valor do KG do Feijão é de: ", vl_feijao, "\n")
 
-print(" ")
+print("O valor do KG do Açúcar é de: ", vl_acucar, "\n")
 
-print(" =======================================  ")
+print("O valor do KG da Batata é de: ", vl_batata, "\n")
 
-print(" ")
+print("O valor do Litro de Óleo é de: ", vl_oleo, "\n")
 
-print("O preço dos produtos são")
+print("O valor do PACOTE de Bolacha de água e sal é de: ", vl_bolachaAS, "\n")
 
-print(" ")
+print("O valor do PACOTE de Bolacha de maizena é de: ", vl_bolachaM, "\n")
+
+print("O valor do MAÇO de Couve-flor é de: ", vl_couve, "\n")
+
+print("O valor da LATA de Leite em Pó é de: ", vl_leitepo, "\n")
+
+
+print(" =======================================  \n")
+
+print("O preço dos produtos são \n")
 
 pr_arroz = vl_arroz * arroz
 pr_feijao = vl_feijao * feijao
@@ -72,42 +56,35 @@ pr_bolachaM = vl_bolachaM * bolacha_maizena
 pr_couve = vl_couve * couve_flor
 pr_leitepo = vl_leitepo * leite_po
 
-print("O preço de 5KG de Arroz é de: ", pr_arroz)
-print(" ")
-print("O preço de 3KG de Feijão é de: ", pr_feijao)
-print(" ")
-print("O preço de 2KG de Açúcar é de: ", pr_acucar)
-print(" ")
-print("O preço de 3KG de Batata é de: ", pr_batata)
-print(" ")
-print("O preço de 5 Litros de Óleo é de: ", pr_oleo)
-print(" ")
-print("O preço de 1 PACOTE de Bolacha de água e sal é de: ", pr_bolachaAS)
-print(" ")
-print("O preço de 1 PACOTE de Bolacha de maizena é de: ", pr_bolachaM)
-print(" ")
-print("O preço de 2 MAÇOS de Couve-Flor é de: ", pr_couve)
-print(" ")
-print("O preço de 2 Latas de 1KG de Leite em Pó é de: ", pr_leitepo)
-print(" ")
+print("O preço de 5KG de Arroz é de: ", pr_arroz, "\n")
+
+print("O preço de 3KG de Feijão é de: ", pr_feijao, "\n")
+
+print("O preço de 2KG de Açúcar é de: ", pr_acucar, "\n")
+
+print("O preço de 3KG de Batata é de: ", pr_batata, "\n")
+
+print("O preço de 5 Litros de Óleo é de: ", pr_oleo, "\n")
+
+print("O preço de 1 PACOTE de Bolacha de água e sal é de: ", pr_bolachaAS, "\n")
+
+print("O preço de 1 PACOTE de Bolacha de maizena é de: ", pr_bolachaM, "\n")
+
+print("O preço de 2 MAÇOS de Couve-Flor é de: ", pr_couve, "\n")
+
+print("O preço de 2 Latas de 1KG de Leite em Pó é de: ", pr_leitepo, "\n")
 
 vl_total = pr_arroz + pr_feijao + pr_acucar + pr_batata + pr_oleo + pr_bolachaAS + pr_bolachaM + pr_couve + pr_leitepo
 
-print(" ")
+print("======================================== \n")
 
-print("O valor total da compra foi de: R$ ", vl_total)
+print("O valor total da compra foi de: R$ ", vl_total, "\n")
 
-print(" ")
+print(" =======================================  \n")
 
-print(" =======================================  ")
-
-print(" ")
-
-print("Recomendado para gastar R$ 200.00")
-
-print(" ")
+print("Recomendado para gastar R$ 200.00 \n")
 
 if(vl_total < 200.00):
-    print("Pagamento em Dinheiro")
+    print("Pagamento em Dinheiro \n")
 else:
-    print("Pagamento em parcelas com uso de cartão")    
+    print("Pagamento em parcelas com uso de cartão \n")     
